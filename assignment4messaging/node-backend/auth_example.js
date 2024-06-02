e9d8c690436f9d657ce4b8976ba5650d91ae9e5e
zr66 <zr66@node.cci.drexel.edu>
2024-06-02 10:06:47 -0400

    assignment 4 - adding backend

---
+++ assignment4messaging/node-backend/auth_example.js
@@ -0,0 +1,8 @@
+const dbHost = 'example.mongo.host';
+const dbPort = '27017';
+const username = 'dbUsername';
+const password = 'dbPassword';
+const authDatabase = 'dbName';
+const databaseName = 'dbName';
+
+module.exports = {dbHost, dbPort, username, password, authDatabase, databaseName}
